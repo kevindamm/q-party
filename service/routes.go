1d3c8d8c9bd83ec9447338e6a801f1cd99afe472
Kevin Damm <github@kevindamm.com>
2024-11-16 23:02:46 -0500

    exploring possible server (HTTP) routes
    preparing for websocket connections

--- service/routes.go
+++ service/routes.go
@@ -34,25 +34,27 @@ func (server *Server) RegisterRoutes() http.Handler {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/", server.LandingPage)
+	e.GET("/", server.LandingPage).Name = "home"
 
-	// TODO flesh this out some more, add callback path and POST handler
-	e.GET("/login", server.GetLoginForm)
+	// TEMPORARY just to view some stats
+	e.GET("/categories", server.ListCategoriesPerYear)
+
+	// websockets endpoints
+	// /join/:room_id (ask to join room, get host/contestant/audience assignment)
+	// /play/:room_id (contestant interface) .Group( player_auth )
+	// /host/:room_id (host interface)       .Group( host_auth )
 
 	// Vue3 app is hosted here, see /app/* within this repo for implementation.
 	// It's effectively static until it routes to lobby or an in-progress game.
-	e.Static("/app", "vuedist")
+	e.Static("/app", "vuedist").Name = "vue3-root"
 	// image, audio and video media for challenges are also under a static path.
-	e.Static("/media", "media")
+	e.Static("/media", "media").Name = "media-root"
 	// And some root-level static files that can be listed individually.
 	e.Static("/favicon.ico", "public/favicon.ico")
 	e.Static("/jarchive.json", "public/jarchive.json")
 
 	// TODO other request handlers
-	// /play/:room_id (contestant interface)
-	// /host/:room_id (host interface)
-	// /view/:room_id (audience interface)
-	// websockets endpoints
+	// /view/:room_id (audience interface via SSE)
 
 	return e
 }
@@ -65,6 +67,7 @@ func (s *Server) LandingPage(ctx echo.Context) error {
 	return ctx.HTML(http.StatusOK, response["message"])
 }
 
-func (s *Server) GetLoginForm(ctx echo.Context) error {
-	return ctx.HTML(http.StatusOK, "TODO host login")
+func (s *Server) ListCategoriesPerYear(ctx echo.Context) error {
+
+	return ctx.HTML(http.StatusOK, "TODO list categories")
 }
